10ce73e6724c3057927fa0c57769cba639a90ea4
JossuEL55 <jossueayala555@gmail.com>
2026-01-12 19:23:17 -0500

    Actualizar Saludo.java

--- Saludo.java
+++ Saludo.java
@@ -1,6 +1,9 @@
-public String generarSaludo(String nombre) {
-    if (nombre == null || nombre.isEmpty()) {
-        return "¡Hola, Invitado!";
+public class Saludo {
+
+    public String generarSaludo(String nombre) {
+        if (nombre == null || nombre.isEmpty()) {
+            return "¡Hola, Invitado!";
+        }
+        return "¡Hola, " + nombre + "!";
     }
-    return "¡Hola, " + nombre + "!";
 }
